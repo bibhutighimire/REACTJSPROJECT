7c942b671ecd6cb7d2555f4ca737ede8b3c2d264
bibhutighimire <ghimirebibhuti@gmail.com>
2020-07-24 12:57:52 -0600

    added first header below:

---
+++ src/ToDoList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+class ToDoList extends React.Component {
+  render()
+  {
+    return (
+      <>
+      <h2>To Do List in progress</h2>
+      </>
+    )
+  };
+}
+
+export default ToDoList;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import ToDoList from './ToDoList';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
+    <ToDoList />
   </React.StrictMode>,
   document.getElementById('root')
 );
